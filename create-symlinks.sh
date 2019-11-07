f7b3f542025865aac8d06f9d1364bc94675b7cfc
Thomas Gies <thomas.gies@idmedia.com>
2019-11-07 12:27:06 +0100

    Räume create-symlinks.sh auf

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-rm ~/.asoundrc
-rm ~/.bash_profile
-rm ~/.bashrc
-rm ~/.tmux.conf
-rm ~/.vimrc
-rm ~/.xinitrc
-rm ~/.Xresources
-rm ~/.xserverrc
-rm ~/.config/i3
-rm ~/.config/powerline
-
-ln -s ~/vc/dot-files/.asoundrc ~/.asoundrc
-ln -s ~/vc/dot-files/.bash_profile ~/.bash_profile
-ln -s ~/vc/dot-files/.bashrc ~/.bashrc
-ln -s ~/vc/dot-files/.tmux.conf ~/.tmux.conf
-ln -s ~/vc/dot-files/_vimrc ~/.vimrc
-ln -s ~/vc/dot-files/.xinitrc ~/.xinitrc
-ln -s ~/vc/dot-files/.Xresources ~/.Xresources
-ln -s ~/vc/dot-files/.xserverrc ~/.xserverrc
-ln -s ~/vc/dot-files/.config/i3 ~/.config/i3
-ln -s ~/vc/dot-files/.config/powerline ~/.config/powerline
+ln -s ~/dot-files/.bashrc ~/.bashrc
+ln -s ~/dot-files/.tmux.conf ~/.tmux.conf
+ln -s ~/dot-files/.vimrc ~/.vimrc
